5b9d4881e2c1cb11cf6bb50640f6b61d7334f15a
gnneto <guilhermeacacio3@gmail.com>
2023-11-24 20:43:14 -0300

    //

--- backend/acesso/models.py
+++ backend/acesso/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 
-# Create your models here.
 
 class Usuarios(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
