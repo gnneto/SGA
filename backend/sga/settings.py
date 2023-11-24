f62a161074abb17502fe5fb6d0843b3f983f74ef
gnneto <guilhermeacacio3@gmail.com>
2023-11-24 20:33:21 -0300

    /DIRETORIOS/TEMPLATES/

--- backend/sga/settings.py
+++ backend/sga/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'acesso',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'sga.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend/src', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
